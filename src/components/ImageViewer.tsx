eb895a5729dd7611b79df29bc0726701d161f456
곽태욱 <gwak2837@gmail.com>
2025-03-05 23:38:57 +0900

    Update ImageViewer.tsx

--- src/components/ImageViewer.tsx
+++ src/components/ImageViewer.tsx
@@ -53,7 +53,7 @@ export default function ImageViewer({ manga }: Props) {
   return (
     <ul className="relative select-none [&_li]:flex [&_li]:justify-center [&_li]:gap-1 [&_img]:h-dvh [&_img]:min-w-0 [&_img]:object-contain [&_img]:select-none [&_img]:aria-hidden:sr-only">
       {showController && (
-        <div className="fixed top-0 border-gray-500 left-0 right-0 bg-black border-b px-safe z-10">
+        <div className="fixed top-0 border-gray-600 left-0 right-0 bg-black/80 backdrop-blur border-b px-safe z-10">
           <div className="p-2">
             <h1 className="text-center line-clamp-3 font-bold">{title}</h1>
           </div>
@@ -87,18 +87,18 @@ export default function ImageViewer({ manga }: Props) {
       })}
       {isTouchMode && <NavigationTouchArea />}
       {showController && (
-        <div className="fixed border-t border-gray-500 select-none bg-black bottom-0 left-0 right-0 px-safe pb-safe">
+        <div className="fixed border-t border-gray-600 select-none bg-black/80 backdrop-blur bottom-0 left-0 right-0 px-safe pb-safe">
           <div className="p-2 md:p-3 grid gap-1">
-            <div className="grid gap-2 ml-2 grid-cols-[1fr_auto]">
+            <div className="grid gap-2 ml-2 mr-1 grid-cols-[1fr_auto]">
               <Slider
                 max={maxImageIndex}
                 onValueCommit={(value) => setCurrentIndex(value)}
                 step={1}
                 value={currentIndex}
               />
-              <button>
+              {/* <button>
                 <IconUndo className="w-5" />
-              </button>
+              </button> */}
             </div>
             <div className="rounded text-center text-xs mx-auto">
               {startPage === endPage ? startPage : `${startPage}-${endPage}`} / {maxImageIndex + 1}
