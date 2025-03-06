0f1348f10c2a26c8dabf983aa84810af304b8574
gwak2837 <gwak2837@gmail.com>
2025-03-07 03:05:49 +0900

    feat: 상단 버튼

--- src/components/ImageViewer/ImageViewer.tsx
+++ src/components/ImageViewer/ImageViewer.tsx
@@ -4,7 +4,9 @@ import useImageNavigation from '@/hook/useImageNavigation'
 import { useNavigationModeStore } from '@/store/controller/navigationMode'
 import { usePageViewStore } from '@/store/controller/pageView'
 import { type Manga } from '@/types/manga'
+import { useRouter } from 'next/navigation'
 import { useEffect, useState } from 'react'
+import { toast } from 'sonner'
 
 import useTouchNavigator from '../../hook/useNavigationTouchArea'
 import Slider from '../Slider'
@@ -25,6 +27,7 @@ export default function ImageViewer({ manga }: Props) {
   const isDoublePage = pageView === 'double'
   const isTouchMode = navMode === 'touch'
   const isScrollMode = navMode === 'scroll'
+  const router = useRouter()
 
   const { currentIndex, setCurrentIndex, prevPage, nextPage } = useImageNavigation({
     maxIndex: maxImageIndex,
@@ -47,12 +50,58 @@ export default function ImageViewer({ manga }: Props) {
     }
   }, [isTouchMode])
 
+  function toggleFullScreen() {
+    if (!document.fullscreenElement) {
+      document.documentElement.requestFullscreen().catch(() => toast.error('전체화면 전환 실패'))
+    } else {
+      document.exitFullscreen().catch(() => toast.error('전체화면 해제 실패'))
+    }
+  }
+
   return (
     <div className="relative">
       {showController && (
-        <div className="fixed top-0 border-gray-500 left-0 right-0 bg-black/70 backdrop-blur border-b px-safe z-10">
-          <div className="p-3">
-            <h1 className="text-center line-clamp-3 font-bold">{title}</h1>
+        <div className="fixed top-0 left-0 right-0 z-10 bg-black/70 backdrop-blur border-b border-gray-500 px-safe">
+          <div className="flex items-center justify-between p-3 [&_button]:rounded-full [&_button]:active:text-gray-500 [&_button]:hover:bg-gray-900">
+            <div className="flex gap-1">
+              <button aria-label="뒤로가기" className="p-2" onClick={() => router.back()}>
+                <svg className="w-6" fill="none" stroke="currentColor" viewBox="0 0 24 24">
+                  <path d="M15 19l-7-7 7-7" strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" />
+                </svg>
+              </button>
+              <button aria-label="창 닫기" className="p-2" onClick={() => window.close()}>
+                <svg className="w-6" fill="none" stroke="currentColor" viewBox="0 0 24 24">
+                  <path d="M6 18L18 6M6 6l12 12" strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" />
+                </svg>
+              </button>
+            </div>
+            <h1 className="flex-1 text-center line-clamp-3 font-bold text-white">{title}</h1>
+            <div className="flex gap-1">
+              <button aria-label="전체화면" className="p-2" onClick={toggleFullScreen}>
+                <svg className="w-6" fill="none" stroke="currentColor" viewBox="0 0 24 24">
+                  <path
+                    d="M4 4h6M4 4v6M20 4h-6M20 4v6M4 20h6M4 20v-6M20 20h-6M20 20v-6"
+                    strokeLinecap="round"
+                    strokeLinejoin="round"
+                    strokeWidth="2"
+                  />
+                </svg>
+              </button>
+              <button aria-label="새로고침" className="p-2" onClick={() => router.refresh()}>
+                <svg
+                  className="w-6"
+                  fill="none"
+                  stroke="currentColor"
+                  strokeLinecap="round"
+                  strokeLinejoin="round"
+                  strokeWidth="2"
+                  viewBox="0 0 24 24"
+                >
+                  <polyline points="23 4 23 10 17 10"></polyline>
+                  <path d="M20.49 15a9 9 0 1 1-2.12-9.36L23 10"></path>
+                </svg>
+              </button>
+            </div>
           </div>
         </div>
       )}
@@ -76,7 +125,7 @@ export default function ImageViewer({ manga }: Props) {
       )}
 
       {showController && (
-        <div className="fixed border-t border-gray-500 select-none bg-black/70 backdrop-blur bottom-0 left-0 right-0 px-safe pb-safe">
+        <div className="fixed bottom-0 left-0 right-0 z-10 bg-black/70 backdrop-blur border-t border-gray-500 px-safe pb-safe">
           <div className="p-4 grid gap-2">
             {isTouchMode && (
               <>
@@ -88,23 +137,26 @@ export default function ImageViewer({ manga }: Props) {
                     value={currentIndex}
                   />
                 </div>
-                <div className="rounded text-center text-xs mx-auto">
+                <div className="mx-auto rounded text-center text-xs">
                   {startPage === endPage ? startPage : `${startPage}-${endPage}`} / {imageCount}
                 </div>
               </>
             )}
             <div className="font-medium whitespace-nowrap flex-wrap justify-center text-sm flex gap-2 text-black [&_button]:rounded-full [&_button]:bg-gray-100 [&_button]:px-2 [&_button]:py-1 [&_button]:hover:bg-white [&_button]:active:bg-gray-400 [&_button]:disabled:bg-gray-400 [&_button]:disabled:text-gray-500 [&_button]:min-w-20">
-              <button onClick={() => setPageView(isDoublePage ? 'single' : 'double')}>
-                {isDoublePage ? '두 쪽' : '한 쪽'} 보기
-              </button>
               <button onClick={() => setNavMode(isTouchMode ? 'scroll' : 'touch')}>
                 {isTouchMode ? '터치' : '스크롤'} 모드
+              </button>
+              <button onClick={() => setPageView(isDoublePage ? 'single' : 'double')}>
+                {isDoublePage ? '두 쪽' : '한 쪽'} 보기
               </button>
               <button
                 disabled={isScrollMode}
                 onClick={() => setTouchOrientation(isHorizontalTouch ? 'vertical' : 'horizontal')}
               >
                 {isHorizontalTouch ? '좌우' : '상하'} 넘기기
+              </button>
+              <button disabled onClick={() => setPageView(isDoublePage ? 'single' : 'double')}>
+                {isTouchMode ? '세로' : '가로'} 맞춤
               </button>
             </div>
           </div>
