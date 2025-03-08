733582e5eaca11cdd3e0d153675241034dc0f325
gwak2837 <gwak2837@gmail.com>
2025-03-08 12:34:03 +0900

    feat: add sentry

--- src/app/global-error.tsx
+++ src/app/global-error.tsx
@@ -2,7 +2,14 @@
 
 import type { ErrorProps } from '@/types/nextjs'
 
+import * as Sentry from '@sentry/nextjs'
+import { useEffect } from 'react'
+
 export default function GlobalError({ error, reset }: ErrorProps) {
+  useEffect(() => {
+    Sentry.captureException(error)
+  }, [error])
+
   return (
     <html>
       <body className="flex items-center justify-center p-2 h-dvh">
