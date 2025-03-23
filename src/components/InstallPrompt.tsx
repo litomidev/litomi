60e0c340dbd70f0b114d0d2efe76a34206854817
gwak2837 <logan.gwak@ridi.com>
2025-03-23 19:09:15 +0900

    fix: text

--- src/components/InstallPrompt.tsx
+++ src/components/InstallPrompt.tsx
@@ -55,36 +55,44 @@ export default function InstallPrompt() {
   return (
     <>
       {isIOS ? (
-        <p className="flex w-fit mx-auto items-center gap-2 p-4 text-center">
-          홈 화면에 추가하려면 Safari의 공유 버튼
-          <svg
-            className="text-foreground w-6"
-            fill="none"
-            stroke="currentColor"
-            strokeLinecap="round"
-            strokeLinejoin="round"
-            strokeWidth="2"
-            viewBox="0 0 24 24"
-          >
-            <path d="M4 12v8a2 2 0 0 0 2 2h12a2 2 0 0 0 2-2v-8"></path>
-            <polyline points="16 6 12 2 8 6"></polyline>
-            <line x1="12" x2="12" y1="2" y2="15"></line>
-          </svg>
-          을 누른 후 {'"'}홈 화면에 추가{'"'}
-          <svg
-            className="text-foreground w-6"
-            fill="none"
-            stroke="currentColor"
-            strokeLinecap="round"
-            strokeLinejoin="round"
-            strokeWidth="2"
-            viewBox="0 0 24 24"
-          >
-            <rect height="18" rx="2" ry="2" width="18" x="3" y="3"></rect>
-            <line x1="12" x2="12" y1="8" y2="16"></line>
-            <line x1="8" x2="16" y1="12" y2="12"></line>
-          </svg>
-          를 선택하세요.
+        <p className="flex flex-wrap justify-center items-center gap-2 w-fit mx-auto p-4 text-sm">
+          홈 화면에 추가하려면 Safari
+          <span className="flex items-center gap-1">
+            공유 버튼
+            <svg
+              className="text-foreground w-6"
+              fill="none"
+              stroke="currentColor"
+              strokeLinecap="round"
+              strokeLinejoin="round"
+              strokeWidth="2"
+              viewBox="0 0 24 24"
+            >
+              <path d="M4 12v8a2 2 0 0 0 2 2h12a2 2 0 0 0 2-2v-8"></path>
+              <polyline points="16 6 12 2 8 6"></polyline>
+              <line x1="12" x2="12" y1="2" y2="15"></line>
+            </svg>
+            을
+          </span>
+          누른 후
+          <span className="flex items-center gap-1">
+            홈 화면에 추가
+            <svg
+              className="text-foreground w-6"
+              fill="none"
+              stroke="currentColor"
+              strokeLinecap="round"
+              strokeLinejoin="round"
+              strokeWidth="2"
+              viewBox="0 0 24 24"
+            >
+              <rect height="18" rx="2" ry="2" width="18" x="3" y="3"></rect>
+              <line x1="12" x2="12" y1="8" y2="16"></line>
+              <line x1="8" x2="16" y1="12" y2="12"></line>
+            </svg>
+            를
+          </span>
+          선택하세요.
         </p>
       ) : (
         <button
--- src/constants/index.ts
+++ src/constants/index.ts
@@ -1,12 +1,13 @@
 import { CANONICAL_URL } from './url'
 
+export const APPLICATION_NAME = 'Litomi - Manga mirror'
 export const SHORT_NAME = 'Litomi'
 
 export const DESCRIPTION =
   'Read manga online for free without any ads. Fast loading and mobile friendly. Update daily with latest chapters.'
 
 export const defaultOpenGraph = {
-  title: SHORT_NAME,
+  title: APPLICATION_NAME,
   description: DESCRIPTION,
   url: CANONICAL_URL,
   siteName: SHORT_NAME,
