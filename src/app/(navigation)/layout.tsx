fa1fb3857698095adc1c248ae46674b3499ad04c
gwak2837 <gwak2837@gmail.com>
2025-03-28 14:00:50 +0900

    feat: icon 추가

--- src/app/(navigation)/layout.tsx
+++ src/app/(navigation)/layout.tsx
@@ -1,6 +1,12 @@
 import type { BaseLayoutProps } from '@/types/nextjs'
 
+import IconBell from '@/components/icons/IconBell'
+import IconBookmark from '@/components/icons/IconBookmark'
+import IconHome from '@/components/icons/IconHome'
 import IconLogo from '@/components/icons/IconLogo'
+import IconPost from '@/components/icons/IconPost'
+import IconSearch from '@/components/icons/IconSearch'
+import SelectableLink from '@/components/SelectableLink'
 // import BellIcon from '@/svg/BellIcon'
 // import BookmarkIcon from '@/svg/BookmarkIcon'
 // import HomeIcon from '@/svg/HomeIcon'
@@ -8,7 +14,7 @@ import IconLogo from '@/components/icons/IconLogo'
 // import SearchIcon from '@/svg/SearchIcon'
 import Link from 'next/link'
 
-// import NavigLink from './NavigLink'
+// import SelectableLink from './SelectableLink'
 // import ProfileLink from './ProfileLink'
 // import PublishButton from './PublishButton'
 
@@ -16,29 +22,29 @@ export default async function Layout({ children }: BaseLayoutProps) {
   return (
     <div className="mx-auto px-safe pb-safe grid max-w-screen-2xl w-fit sm:flex">
       <header
-        className="fixed bottom-0 z-50 px-safe pb-safe grid w-full grid-cols-[4fr_1fr] overflow-y-auto border-t-2 border-zinc-800 bg-background/70 backdrop-blur
+        className="fixed bottom-0 z-50 px-safe pb-safe grid w-full grid-cols-[4fr_1fr] overflow-y-auto border-t-2 border-zinc-700 bg-background/70 backdrop-blur
           sm:inset-auto sm:flex sm:h-full sm:w-20 sm:flex-col sm:justify-between sm:gap-8 sm:border-r-2 sm:border-t-0 sm:p-2
           2xl:w-3xs"
       >
         <nav className="grid grid-cols-4 whitespace-nowrap sm:grid-cols-none sm:gap-2 xl:text-xl xl:leading-6">
-          <Link className="p-2 w-fit mx-auto focus:outline-none" href="/mangas/id/desc/1">
+          <Link className="p-2 w-fit mx-auto focus:outline-none hidden sm:block" href="/mangas/id/desc/1">
             <IconLogo className="w-8" />
           </Link>
-          {/* <NavigLink Icon={HomeIcon} href={`/${locale}/exam`}>
-            {dict.홈[locale]}
-          </NavigLink>
-          <NavigLink Icon={SearchIcon} href={`/${locale}/search`}>
-            {dict.검색[locale]}
-          </NavigLink>
-          <NavigLink Icon={PostIcon} href={`/${locale}/post`}>
-            {dict.글[locale]}
-          </NavigLink>
-          <NavigLink Icon={BellIcon} href={`/${locale}/notification`}>
-            {dict.알림[locale]}
-          </NavigLink>
-          <NavigLink Icon={BookmarkIcon} className="hidden sm:block" href={`/${locale}/bookmark`}>
-            {dict.북마크[locale]}
-          </NavigLink> */}
+          <SelectableLink href="/mangas/id/desc/1" Icon={IconHome}>
+            홈
+          </SelectableLink>
+          <SelectableLink href="/search" Icon={IconSearch}>
+            검색
+          </SelectableLink>
+          <SelectableLink href="/post" Icon={IconPost}>
+            글
+          </SelectableLink>
+          <SelectableLink href="/notification" Icon={IconBell}>
+            알림
+          </SelectableLink>
+          <SelectableLink className="hidden sm:block" href="/bookmark" Icon={IconBookmark}>
+            북마크
+          </SelectableLink>
           {/* <PublishButton /> */}
         </nav>
         {/* <ProfileLink /> */}
---
+++ src/components/icons/IconBookmark.tsx
@@ -0,0 +1,21 @@
+'use client'
+
+type Props = {
+  className?: string
+  selected: boolean
+}
+
+export default function IconBookmark({ className, selected }: Props) {
+  return (
+    <svg
+      className={className}
+      fill={selected ? 'currentColor' : 'none'}
+      stroke="currentColor"
+      strokeLinejoin="round"
+      strokeWidth="2"
+      viewBox="0 0 24 24"
+    >
+      <path d="M19 21l-7-5-7 5V5a2 2 0 0 1 2-2h10a2 2 0 0 1 2 2z"></path>
+    </svg>
+  )
+}
