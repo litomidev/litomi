7053c5d87219643316236676ae096fb3b3c45cc0
gwak2837 <gwak2837@gmail.com>
2025-03-29 10:17:34 +0900

    WIP: fix: profile

--- src/app/(navigation)/Profile.tsx
+++ src/app/(navigation)/Profile.tsx
@@ -2,10 +2,10 @@
 
 import Loading from '@/components/ui/Loading'
 import { SessionStorageKey } from '@/constants/storage'
-import { locale } from 'dayjs'
 import Link from 'next/link'
 import { usePathname } from 'next/navigation'
-import { useActionState } from 'react'
+import { useActionState, useEffect } from 'react'
+import { toast } from 'sonner'
 
 import IconLogin from '../../components/icons/IconLogin'
 import IconLogout from '../../components/icons/IconLogout'
@@ -20,8 +20,15 @@ const initialState = {
 
 export default function Profile() {
   const pathname = usePathname()
-  const accessToken = ''
-  const [{ success }, formAction, pending] = useActionState(logout, initialState)
+  const accessToken = '2'
+  const [logoutState, formAction, pending] = useActionState(logout, initialState)
+
+  useEffect(() => {
+    const { error } = logoutState
+    if (error) {
+      toast.error(error)
+    }
+  }, [logoutState])
 
   const user = {
     id: '1',
@@ -32,27 +39,28 @@ export default function Profile() {
 
   return accessToken ? (
     <Link
-      className="relative flex items-center justify-center rounded-full sm:px-2 sm:py-4 xl:px-0"
-      href={`/${locale}/@${user.loginId}`}
+      className="relative flex items-center justify-center rounded-full sm:px-2 sm:py-4 2xl:px-0"
+      href={`/@${user.loginId}`}
     >
       <Squircle src={user.profileImageURLs?.[0]} wrapperClassName="w-8 flex-shrink-0 sm:w-10">
         {user.nickname.slice(0, 2)}
       </Squircle>
-      <div className="ml-3 hidden w-full min-w-0 gap-1 py-0.5 xl:grid">
-        <div className="hidden overflow-hidden whitespace-nowrap leading-5 xl:block">{user.nickname}</div>
-        <div className="dark:text-midnight-400 text-midnight-300 hidden overflow-hidden text-ellipsis whitespace-nowrap leading-5 xl:block">
-          @{user.loginId}
-        </div>
+      <div className="ml-3 hidden w-full min-w-0 gap-1 py-0.5 2xl:grid">
+        <div className="overflow-hidden whitespace-nowrap leading-5">{user.nickname}</div>
+        <div className="text-zinc-400 overflow-hidden text-ellipsis whitespace-nowrap leading-5">@{user.loginId}</div>
       </div>
-      <form action={formAction}>
-        <label className="relative" onClick={(e) => e.stopPropagation()}>
+      <form action={formAction} className="relative whitespace-nowrap" onClick={(e) => e.stopPropagation()}>
+        <label>
           <input
-            className="hover:bg-midnight-500/10 hover:dark:bg-midnight-500/40 peer absolute inset-0 appearance-none overflow-hidden rounded-full transition-colors"
+            className="hover:bg-zinc-500/40 peer absolute inset-0 appearance-none overflow-hidden rounded-full transition"
             type="checkbox"
           />
-          <div className="pointer-events-none absolute -top-6 right-0 z-10 hidden w-[271px] -translate-y-full rounded-2xl border bg-white p-4 opacity-0 shadow-lg transition-opacity peer-checked:pointer-events-auto peer-checked:opacity-100 xl:block dark:bg-gray-800">
+          <div
+            className="pointer-events-none absolute -top-6 right-0 z-10 -translate-y-full rounded-2xl border p-4 opacity-0 shadow-lg transition 
+            peer-checked:pointer-events-auto peer-checked:opacity-100 bg-zinc-900 "
+          >
             <button
-              className="group flex w-fit items-center gap-5 rounded-full p-3 text-red-500 transition-colors hover:bg-red-500/10 focus-visible:outline focus-visible:outline-red-600 active:scale-90 disabled:text-gray-500 disabled:hover:bg-inherit disabled:active:scale-100 xl:m-0 hover:dark:bg-red-500/20 focus:dark:outline-red-200 disabled:dark:text-gray-400"
+              className="group m-0 flex w-fit items-center gap-5 rounded-full p-3 text-red-500 transition-colors hover:bg-red-500/10 focus-visible:outline focus-visible:outline-red-600 active:scale-90 disabled:text-gray-500 disabled:hover:bg-inherit disabled:active:scale-100 hover:dark:bg-red-500/20 focus:dark:outline-red-200 disabled:dark:text-gray-400"
               disabled={pending}
             >
               {pending ? (
@@ -60,12 +68,12 @@ export default function Profile() {
               ) : (
                 <>
                   <IconLogout className="w-6 transition-transform group-hover:scale-110 group-disabled:scale-100" />
-                  <span className="hidden min-w-0 xl:block">로그아웃</span>
+                  <span className="min-w-0">로그아웃</span>
                 </>
               )}
             </button>
           </div>
-          <IconMore className="relative hidden w-11 cursor-pointer p-3 xl:block" />
+          <IconMore className="relative hidden w-11 cursor-pointer p-3 2xl:block" />
         </label>
       </form>
     </Link>
@@ -85,9 +93,9 @@ export function ProfileSkeleton() {
   return (
     <div className="flex items-center justify-center sm:py-4">
       <Squircle wrapperClassName="w-8 animate-pulse flex-shrink-0 sm:w-10" />
-      <div className="mx-3 hidden w-full min-w-0 gap-1 py-0.5 xl:grid">
-        <div className="h-5 animate-pulse rounded-xl bg-gray-200 xl:block dark:bg-gray-800" />
-        <div className="h-5 animate-pulse rounded-xl bg-gray-200 xl:block dark:bg-gray-800" />
+      <div className="mx-3 hidden w-full min-w-0 gap-1 py-0.5 2xl:grid">
+        <div className="h-5 animate-pulse rounded-xl dark:bg-zinc-800" />
+        <div className="h-5 animate-pulse rounded-xl dark:bg-zinc-800" />
       </div>
     </div>
   )
