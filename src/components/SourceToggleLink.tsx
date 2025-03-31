16d148f764a01cde0bca090bcf5bfd45dd3f8795
gwak2837 <gwak2837@gmail.com>
2025-04-01 01:58:56 +0900

    fix: routing prevent

--- src/components/SourceToggleLink.tsx
+++ src/components/SourceToggleLink.tsx
@@ -10,11 +10,11 @@ export default function SourceToggleLink({ currentSource }: OrderToggleProps) {
   return (
     <div
       className="relative grid grid-cols-2 bg-zinc-900 border-2 border-zinc-800 p-1 rounded-xl text-zinc-400 text-sm md:text-base
-        [&_a]:relative [&_a]:rounded [&_a]:px-3 [&_a]:py-1 [&_a]:aria-current:font-bold [&_a]:aria-current:text-foreground"
+        [&_a]:relative [&_a]:rounded [&_a]:px-3 [&_a]:py-1 [&_a]:aria-current:font-bold [&_a]:aria-current:text-foreground [&_a]:aria-current:pointer-events-none"
     >
       <Link aria-current={currentSource === 'ha'} href="ha">
         <div
-          className="absolute inset-0 bg-zinc-800 rounded-lg border-2 border-zinc-700"
+          className="absolute inset-0 bg-zinc-800 rounded-lg border-2 border-zinc-700 pointer-events-none"
           style={{ transform: `translateX(${100 * selectedOrderIndex}%)` }}
         />
         <span className="relative">ha</span>
