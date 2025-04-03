0f5eeca72b53a55d7e14d0d42efca4059c5280e7
gwak2837 <gwak2837@gmail.com>
2025-04-03 21:24:26 +0900

    feat: 로그인 유지

--- src/app/auth/login/LoginForm.tsx
+++ src/app/auth/login/LoginForm.tsx
@@ -51,7 +51,7 @@ export default function LoginForm() {
   return (
     <form
       action={formAction}
-      className="grid gap-6 
+      className="grid gap-5 
         [&_label]:block [&_label]:mb-1.5 [&_label]:text-sm [&_label]:font-medium [&_label]:text-zinc-300
         [&_input]:w-full [&_input]:rounded-md [&_input]:bg-zinc-800 [&_input]:border [&_input]:border-zinc-600 
         [&_input]:px-3 [&_input]:py-2 [&_input]:placeholder-zinc-500 [&_input]:focus:outline-none [&_input]:focus:ring-2 [&_input]:focus:ring-zinc-500 
@@ -102,9 +102,27 @@ export default function LoginForm() {
             </button>
           </div>
         </div>
+        <label className="!flex w-fit ml-auto items-center gap-2 cursor-pointer">
+          <input
+            className="sr-only peer"
+            defaultChecked={Boolean(formData?.get('remember'))}
+            disabled={pending}
+            id="remember"
+            name="remember"
+            type="checkbox"
+          />
+          <span className="text-sm text-zinc-400 select-none transition peer-checked:text-foreground">
+            로그인 상태 유지
+          </span>
+          <div className="relative transition duration-300 peer-checked:[&>span]:translate-x-full peer-checked:[&>div]:bg-brand-gradient peer-active:[&>div]:ring-2">
+            <div className="w-10 h-6 bg-zinc-600 ring-zinc-500 rounded-full transition duration-300" />
+            <span className="absolute left-1 top-1 w-4 h-4 bg-foreground border border-zinc-300 rounded-full transform transition duration-300" />
+          </div>
+        </label>
       </div>
       <button
-        className="group border-2 border-brand-gradient font-medium rounded-xl disabled:border-zinc-500 disabled:pointer-events-none disabled:text-zinc-500 focus:outline-none focus:ring-3 focus:ring-zinc-500"
+        className="group border-2 border-brand-gradient font-medium rounded-xl focus:outline-none focus:ring-3 focus:ring-zinc-500
+        disabled:border-zinc-500 disabled:pointer-events-none disabled:text-zinc-500"
         disabled={pending}
         type="submit"
       >
@@ -120,63 +138,5 @@ export default function LoginForm() {
 }
 
 export function LoginFormSkeleton() {
-  return (
-    <form
-      className="grid gap-6 
-        [&_label]:block [&_label]:mb-1.5 [&_label]:text-sm [&_label]:font-medium [&_label]:text-zinc-300
-        [&_input]:w-full [&_input]:rounded-md [&_input]:bg-zinc-800 [&_input]:border [&_input]:border-zinc-600 
-        [&_input]:px-3 [&_input]:py-2 [&_input]:placeholder-zinc-500 [&_input]:focus:outline-none [&_input]:focus:ring-2 [&_input]:focus:ring-zinc-500 
-        [&_input]:focus:border-transparent"
-    >
-      <div
-        className="grid gap-4 [&_button]:absolute [&_button]:top-1/2 [&_button]:right-2 [&_button]:-translate-y-1/2
-          [&_button]:rounded-full [&_button]:p-1 [&_button]:bg-zinc-700 [&_button]:text-zinc-400 [&_button]:hover:bg-zinc-600"
-      >
-        <div>
-          <label htmlFor="loginId">아이디</label>
-          <div className="relative">
-            <input
-              autoFocus
-              id="loginId"
-              maxLength={32}
-              minLength={2}
-              name="loginId"
-              pattern={loginIdPattern}
-              placeholder="아이디를 입력하세요"
-              required
-            />
-            <button tabIndex={-1} type="button">
-              <IconX className="w-3.5" />
-            </button>
-          </div>
-        </div>
-        <div>
-          <label htmlFor="password">비밀번호</label>
-          <div className="relative">
-            <input
-              id="password"
-              maxLength={64}
-              minLength={8}
-              name="password"
-              pattern={passwordPattern}
-              placeholder="비밀번호를 입력하세요"
-              required
-              type="password"
-            />
-            <button tabIndex={-1} type="button">
-              <IconX className="w-3.5" />
-            </button>
-          </div>
-        </div>
-      </div>
-      <button
-        className="group border-2 border-brand-gradient font-medium rounded-xl focus:outline-none focus:ring-3 focus:ring-zinc-500"
-        type="submit"
-      >
-        <div className="p-2 flex justify-center bg-zinc-900 rounded-xl hover:bg-zinc-800 transition active:bg-zinc-900">
-          로그인
-        </div>
-      </button>
-    </form>
-  )
+  return <div className="h-[262px] rounded-xl bg-zinc-700 animate-fade-in duration-1000" />
 }
