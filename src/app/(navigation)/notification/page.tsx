999d5b4ff018be72a599a6970614c72f1ecd6ca6
gwak2837 <gwak2837@gmail.com>
2025-04-04 10:05:00 +0900

    fix: sonarqube

--- src/app/(navigation)/notification/page.tsx
+++ src/app/(navigation)/notification/page.tsx
@@ -1,11 +1,6 @@
-import type { BasePageProps } from '@/types/nextjs'
-
-import MangaCard from '@/components/card/MangaCard'
-import { hashaMangaIdsDesc, hashaMangas } from '@/database/hasha'
-
 export const dynamic = 'error'
 
-export default async function Page({ params }: BasePageProps) {
+export default async function Page() {
   return (
     <div className="p-2">
       <h1 className="text-lg font-bold">준비 중입니다</h1>
