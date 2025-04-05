0a8cd5042b403fdc6e0174e1a070ebf9e46339a7
gwak2837 <gwak2837@gmail.com>
2025-04-05 17:27:58 +0900

    fix: style

--- src/app/(navigation)/(right-search)/layout.tsx
+++ src/app/(navigation)/(right-search)/layout.tsx
@@ -4,7 +4,7 @@ export default function Layout({ children }: BaseLayoutProps) {
   return (
     <div className="flex min-h-full lg:gap-8 2xl:gap-10">
       <div className="flex flex-col grow lg:border-r-2">{children}</div>
-      <div className="hidden text-center lg:block w-[300px] shrink-0 bg-zinc-800">검색</div>
+      <div className="hidden text-center lg:block w-2xs shrink-0 bg-zinc-800">검색</div>
     </div>
   )
 }
--- src/app/(navigation)/layout.tsx
+++ src/app/(navigation)/layout.tsx
@@ -15,7 +15,7 @@ import Profile, { ProfileError, ProfileSkeleton } from '../../components/header/
 
 export default async function Layout({ children }: BaseLayoutProps) {
   return (
-    <div className="flex flex-col h-full mx-auto px-safe pb-safe max-w-screen-2xl sm:flex-row">
+    <div className="flex flex-col min-h-full mx-auto px-safe pb-safe max-w-screen-2xl sm:flex-row">
       <header
         className="fixed bottom-0 left-0 right-0 z-50 m-auto px-safe pb-safe grid grid-cols-[4fr_1fr] border-t-2 bg-background/70 backdrop-blur
           sm:inset-auto sm:flex sm:h-full sm:w-20 sm:flex-col sm:justify-between sm:gap-8 sm:border-r-2 sm:border-t-0 sm:p-2
