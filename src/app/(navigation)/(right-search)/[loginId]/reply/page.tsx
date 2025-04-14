415b3f02dd617b3463055a18c3663ccbea68eb82
gwak2837 <gwak2837@gmail.com>
2025-04-15 00:28:25 +0900

    chore: a

--- src/app/(navigation)/(right-search)/[loginId]/reply/page.tsx
+++ src/app/(navigation)/(right-search)/[loginId]/reply/page.tsx
@@ -4,6 +4,9 @@ import Image from 'next/image'
 import Link from 'next/link'
 import { notFound } from 'next/navigation'
 
+// TEMP
+export const dynamic = 'error'
+
 export default async function Page({ params }: BasePageProps) {
   notFound()
 }
