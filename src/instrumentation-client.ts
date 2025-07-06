96485ccdb5f53c2e9dcbb0b7f56f383e9d3e1e35
gwak2837 <gwak2837@gmail.com>
2025-07-06 23:13:37 +0900

    feat: sentry reinstall
    WIP sentry reinstall

--- src/instrumentation-client.ts
+++ src/instrumentation-client.ts
@@ -1,17 +1,20 @@
 // This file configures the initialization of Sentry on the client.
-// The config you add here will be used whenever a users loads a page in their browser.
+// The added config here will be used whenever a users loads a page in their browser.
 // https://docs.sentry.io/platforms/javascript/guides/nextjs/
 
 import * as Sentry from '@sentry/nextjs'
 
 Sentry.init({
   dsn: 'https://40e7a8fce2e957f313b4bbe7b9c46a2e@o4506216356511744.ingest.us.sentry.io/4508939892097024',
-  environment: process.env.NEXT_PUBLIC_VERCEL_ENV || 'local',
-  sendDefaultPii: true,
 
   // Define how likely traces are sampled. Adjust this value in production, or use tracesSampler for greater control.
   tracesSampleRate: 1,
 
   // Setting this option to true will print useful information to the console while you're setting up Sentry.
   debug: false,
+
+  environment: process.env.NEXT_PUBLIC_VERCEL_ENV || 'local',
+  sendDefaultPii: true,
 })
+
+export const onRouterTransitionStart = Sentry.captureRouterTransitionStart
