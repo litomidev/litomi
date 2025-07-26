8b9e28b6070bee827d92595023c42347afa270a0
gwak2837 <gwak2837@gmail.com>
2025-07-26 13:13:06 +0900

    feat: 로컬 db 생성

--- src/database/schema.ts
+++ src/database/schema.ts
@@ -8,6 +8,7 @@ export const userTable = pgTable('user', {
   loginId: varchar('login_id', { length: 32 }).notNull().unique(),
   passwordHash: text('password_hash').notNull(),
   nickname: varchar({ length: 32 }).notNull(),
+  name: varchar({ length: 32 }),
   imageURL: varchar('image_url', { length: 256 }),
 }).enableRLS()
 
