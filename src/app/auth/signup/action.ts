ce8dd9fd6cb8647c9211182a827c3a7a645cf58e
gwak2837 <gwak2837@gmail.com>
2025-07-29 20:13:27 +0900

    refactor: zod 공통화

--- src/app/auth/signup/action.ts
+++ src/app/auth/signup/action.ts
@@ -7,27 +7,17 @@ import { z } from 'zod/v4'
 import { SALT_ROUNDS } from '@/constants'
 import { db } from '@/database/drizzle'
 import { userTable } from '@/database/schema'
+import { loginIdSchema, nicknameSchema, passwordSchema } from '@/database/zod'
 import { setAccessTokenCookie, setRefreshTokenCookie } from '@/utils/cookie'
 import { createFormError, FormError, FormErrors, zodToFormError } from '@/utils/form-error'
 import { generateRandomNickname, generateRandomProfileImage } from '@/utils/nickname'
 
 const schema = z
   .object({
-    loginId: z
-      .string()
-      .min(2, { error: '아이디는 최소 2자 이상이어야 해요' })
-      .max(32, { error: '아이디는 최대 32자까지 입력할 수 있어요' })
-      .regex(/^[a-zA-Z][a-zA-Z0-9-._~]+$/, { error: '아이디는 알파벳, 숫자 - . _ ~ 로만 구성해야 해요' }),
-    password: z
-      .string()
-      .min(8, { error: '비밀번호는 최소 8자 이상이어야 해요' })
-      .max(64, { error: '비밀번호는 최대 64자까지 입력할 수 있어요' })
-      .regex(/^(?=.*[A-Za-z])(?=.*\d).+$/, { error: '비밀번호는 알파벳과 숫자를 하나 이상 포함해야 해요' }),
+    loginId: loginIdSchema,
+    password: passwordSchema,
     'password-confirm': z.string(),
-    nickname: z
-      .string()
-      .min(2, { error: '닉네임은 최소 2자 이상이어야 해요' })
-      .max(32, { error: '닉네임은 최대 32자까지 입력할 수 있어요' }),
+    nickname: nicknameSchema,
   })
   .refine((data) => data.password === data['password-confirm'], {
     error: '비밀번호와 비밀번호 확인 값이 일치하지 않아요',
