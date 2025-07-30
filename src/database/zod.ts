0e394f263471157b7519d109bd02ce1c0b19d6c7
gwak2837 <gwak2837@gmail.com>
2025-07-30 20:49:33 +0900

    Update zod.ts

--- src/database/zod.ts
+++ src/database/zod.ts
@@ -5,7 +5,7 @@ export const loginIdSchema = z
   .string()
   .min(2, { error: '아이디는 최소 2자 이상이어야 해요' })
   .max(32, { error: '아이디는 최대 32자까지 입력할 수 있어요' })
-  .regex(/^[a-zA-Z][a-zA-Z0-9-._~]+$/, { error: '아이디는 알파벳, 숫자 - . _ ~ 로만 구성해야 해요' })
+  .regex(/^[a-zA-Z][a-zA-Z0-9_]+$/, { error: '아이디는 알파벳, 숫자, _ 로만 구성해야 해요' })
 
 export const passwordSchema = z
   .string()
@@ -17,8 +17,13 @@ export const nameSchema = z
   .string()
   .min(2, { error: '이름은 최소 2자 이상이어야 해요' })
   .max(32, { error: '이름은 최대 32자까지 입력할 수 있어요' })
+  .regex(/^[a-zA-Z][a-zA-Z0-9-._~]+$/, { error: '이름은 알파벳, 숫자 - . _ ~ 로만 구성해야 해요' })
 
 export const nicknameSchema = z
   .string()
   .min(2, { error: '닉네임은 최소 2자 이상이어야 해요' })
   .max(32, { error: '닉네임은 최대 32자까지 입력할 수 있어요' })
+
+export const imageURLSchema = z
+  .url('프로필 이미지 주소가 URL 형식이 아니에요')
+  .max(256, '프로필 이미지 URL은 최대 256자까지 입력할 수 있어요')
