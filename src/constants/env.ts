41e06f4ee036aa98569648a2384d432ff3ba16ee
gwak2837 <gwak2837@gmail.com>
2025-08-02 00:37:06 +0900

    feat: service worker

--- src/constants/env.ts
+++ src/constants/env.ts
@@ -3,7 +3,7 @@ import { CANONICAL_URL } from './url'
 export const GA_ID = process.env.GA_ID
 export const GTM_ID = process.env.GTM_ID
 export const AMPLITUDE_API_KEY = process.env.AMPLITUDE_API_KEY
-export const NEXT_PUBLIC_VAPID_PUBLIC_KEY = process.env.NEXT_PUBLIC_VAPID_PUBLIC_KEY
+export const NEXT_PUBLIC_VAPID_PUBLIC_KEY = process.env.NEXT_PUBLIC_VAPID_PUBLIC_KEY ?? ''
 export const NEXT_PUBLIC_BACKEND_URL = process.env.NEXT_PUBLIC_BACKEND_URL
 export const POSTGRES_URL = process.env.POSTGRES_URL ?? ''
 export const JWT_SECRET_ACCESS_TOKEN = process.env.JWT_SECRET_ACCESS_TOKEN ?? ''
@@ -14,3 +14,4 @@ export const VERCEL_SPEED_INSIGHTS = process.env.VERCEL_SPEED_INSIGHTS ?? ''
 export const WEBAUTHN_ORIGIN = process.env.NODE_ENV === 'production' ? CANONICAL_URL : 'http://localhost:3000'
 export const WEBAUTHN_RP_ID = process.env.NODE_ENV === 'production' ? new URL(CANONICAL_URL).hostname : 'localhost'
 export const WEBAUTHN_RP_NAME = 'litomi'
+export const VAPID_PRIVATE_KEY = process.env.VAPID_PRIVATE_KEY ?? ''
