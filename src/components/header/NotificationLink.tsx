9e4010ee037ad8a050693218f07340bb6904628a
gwak2837 <gwak2837@gmail.com>
2025-08-07 23:42:50 +0900

    Update NotificationLink.tsx

--- src/components/header/NotificationLink.tsx
+++ src/components/header/NotificationLink.tsx
@@ -22,7 +22,7 @@ export default function NotificationLink({ className }: Readonly<Props>) {
       {unreadCount > 0 && (
         <span
           className="absolute top-1/2 -translate-y-5 right-1/2 translate-x-5 flex h-4 px-1 items-center justify-center rounded-full bg-brand-end text-[10px] font-bold text-background
-          2xl:right-2 2xl:top-1/2 2xl:-translate-y-1/2 2xl:text-xs 2xl:h-5 2xl:px-2"
+          2xl:right-2 2xl:top-1/2 2xl:-translate-y-1/2 2xl:translate-x-0 2xl:text-xs 2xl:h-5 2xl:px-2"
         >
           {unreadCount > 9 ? '9+' : unreadCount}
         </span>
