594b3e1b14b0bf53c6a8044ab00ca6077c242b4e
gwak2837 <gwak2837@gmail.com>
2025-08-11 09:13:44 +0900

    fix: RLS

--- src/database/manga-schema.ts
+++ src/database/manga-schema.ts
@@ -7,64 +7,64 @@ export const mangaTable = pgTable('manga', {
   title: text().notNull(),
   type: smallint().notNull(),
   count: smallint(),
-})
+}).enableRLS()
 
 export const mangaArtistTable = pgTable('manga_artist', {
   mangaId: integer().references(() => mangaTable.id),
   artistId: integer().references(() => artistTable.id),
-})
+}).enableRLS()
 
 export const artistTable = pgTable('artist', {
   id: integer().primaryKey(),
   value: text().notNull(),
-})
+}).enableRLS()
 
 export const mangaCharacterTable = pgTable('manga_character', {
   mangaId: integer().references(() => mangaTable.id),
   characterId: integer().references(() => characterTable.id),
-})
+}).enableRLS()
 
 export const characterTable = pgTable('character', {
   id: integer().primaryKey(),
   value: text().notNull(),
-})
+}).enableRLS()
 
 export const mangaTagTable = pgTable('manga_tag', {
   mangaId: integer().references(() => mangaTable.id),
   tagId: integer().references(() => tagTable.id),
-})
+}).enableRLS()
 
 export const tagTable = pgTable('tag', {
   id: integer().primaryKey(),
   value: text().notNull(),
-})
+}).enableRLS()
 
 export const mangaSeriesTable = pgTable('manga_series', {
   mangaId: integer().references(() => mangaTable.id),
   seriesId: integer().references(() => seriesTable.id),
-})
+}).enableRLS()
 
 export const seriesTable = pgTable('series', {
   id: integer().primaryKey(),
   value: text().notNull(),
-})
+}).enableRLS()
 
 export const mangaGroupTable = pgTable('manga_group', {
   mangaId: integer().references(() => mangaTable.id),
   groupId: integer().references(() => groupTable.id),
-})
+}).enableRLS()
 
 export const groupTable = pgTable('group', {
   id: integer().primaryKey(),
   value: text().notNull(),
-})
+}).enableRLS()
 
 export const mangaLanguageTable = pgTable('manga_language', {
   mangaId: integer().references(() => mangaTable.id),
   languageId: smallint().references(() => languageTable.id),
-})
+}).enableRLS()
 
 export const languageTable = pgTable('language', {
   id: smallint().primaryKey(),
   value: text().notNull(),
-})
+}).enableRLS()
