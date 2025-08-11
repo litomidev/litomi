1daa3f871def7055daef1a362d401536f21f93d2
gwak2837 <gwak2837@gmail.com>
2025-08-11 09:34:56 +0900

    fix: Lighthouse

--- src/components/ui/Toggle.tsx
+++ src/components/ui/Toggle.tsx
@@ -6,7 +6,7 @@ interface Props extends Omit<ComponentProps<'input'>, 'onChange' | 'onToggle' | 
 
 export default function Toggle({ className = '', onToggle, ...props }: Readonly<Props>) {
   return (
-    <label className="inline-flex cursor-pointer">
+    <label className="inline-flex cursor-pointer items-center">
       <input {...props} className="sr-only peer" onChange={(e) => onToggle?.(e.target.checked)} type="checkbox" />
       <span
         className={`relative aspect-[2/1] bg-zinc-500 rounded-full border box-content transition peer-focus-visible:ring-2 peer-focus-visible:ring-offset-2 after:content-[''] after:absolute after:inset-y-[10%] after:left-[10%] after:w-[40%] after:bg-foreground after:border after:border-zinc-300 after:rounded-full after:transition after:shadow-sm peer-checked:after:translate-x-[100%] ${className}`}
