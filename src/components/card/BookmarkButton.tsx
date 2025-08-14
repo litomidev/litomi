517a91f5159a149e34fbf4e0bb10bbe9a7ed1096
gwak2837 <gwak2837@gmail.com>
2025-08-14 18:52:42 +0900

    refactor: 북마크 추가/삭제 버튼

--- src/components/card/BookmarkButton.tsx
+++ src/components/card/BookmarkButton.tsx
@@ -3,21 +3,19 @@
 import { captureException } from '@sentry/nextjs'
 import { ErrorBoundaryFallbackProps } from '@suspensive/react'
 import { useQueryClient } from '@tanstack/react-query'
-import { useActionState, useEffect, useMemo } from 'react'
+import { useEffect, useMemo } from 'react'
 import { toast } from 'sonner'
 
 import toggleBookmark from '@/app/(navigation)/(right-search)/[name]/bookmark/action'
 import { GETBookmarksResponse } from '@/app/api/bookmark/route'
 import { QueryKeys } from '@/constants/query'
-import useActionErrorEffect from '@/hook/useActionErrorEffect'
+import useActionResponse from '@/hook/useActionResponse'
 import useBookmarksQuery from '@/query/useBookmarksQuery'
 import useMeQuery from '@/query/useMeQuery'
 import { Manga } from '@/types/manga'
 
 import IconBookmark from '../icons/IconBookmark'
 import LoginLink from '../LoginLink'
-
-const initialState = {} as Awaited<ReturnType<typeof toggleBookmark>>
 
 type BookmarkButtonSkeletonProps = {
   className?: string
@@ -32,51 +30,53 @@ export default function BookmarkButton({ manga, className }: Readonly<Props>) {
   const { id: mangaId } = manga
   const { data: me } = useMeQuery()
   const { data: bookmarks } = useBookmarksQuery()
-  const [{ status, data, message }, formAction, isPending] = useActionState(toggleBookmark, initialState)
   const bookmarkIds = useMemo(() => new Set(bookmarks?.bookmarks.map((bookmark) => bookmark.mangaId)), [bookmarks])
   const isIconSelected = bookmarkIds.has(mangaId)
   const queryClient = useQueryClient()
 
-  useActionErrorEffect({
-    status,
-    error: message,
-    onError: (error) => toast.error(error),
-  })
-
-  useEffect(() => {
-    if (!data) {
-      return
-    }
-
-    // NOTE: isBookmarked=true 일 때 createdAt 항상 있음
-    const { isBookmarked, createdAt = 0 } = data
-    toast.success(data.isBookmarked ? '북마크를 추가했어요' : '북마크를 삭제했어요')
-
-    queryClient.setQueryData<GETBookmarksResponse>(QueryKeys.bookmarks, (oldBookmarks) => {
-      const newBookmark = { mangaId, createdAt }
-
-      if (!oldBookmarks) {
-        return {
-          bookmarks: [newBookmark],
-          nextCursor: null,
-        }
-      } else if (isBookmarked) {
-        return {
-          bookmarks: [newBookmark, ...oldBookmarks.bookmarks],
-          nextCursor: null,
-        }
-      } else {
-        return {
-          bookmarks: oldBookmarks.bookmarks.filter((bookmark) => bookmark.mangaId !== mangaId),
-          nextCursor: null,
-        }
+  const [_, dispatchAction, isPending] = useActionResponse({
+    action: toggleBookmark,
+    onError: (error) => {
+      if (typeof error === 'string') {
+        toast.error(error)
       }
-    })
+    },
+    onSuccess: ({ mangaId, createdAt }) => {
+      const isBookmarked = Boolean(createdAt)
 
-    if (isBookmarked) {
-      queryClient.invalidateQueries({ queryKey: QueryKeys.infiniteBookmarks })
-    }
-  }, [data, queryClient, mangaId])
+      if (isBookmarked) {
+        toast.success('북마크를 추가했어요')
+      } else {
+        toast.success('북마크를 삭제했어요')
+      }
+
+      queryClient.setQueryData<GETBookmarksResponse>(QueryKeys.bookmarks, (oldBookmarks) => {
+        const newBookmark = { mangaId, createdAt: createdAt ? new Date(createdAt).getTime() : 0 }
+
+        if (!oldBookmarks) {
+          return {
+            bookmarks: [newBookmark],
+            nextCursor: null,
+          }
+        } else if (isBookmarked) {
+          return {
+            bookmarks: [newBookmark, ...oldBookmarks.bookmarks],
+            nextCursor: null,
+          }
+        } else {
+          return {
+            bookmarks: oldBookmarks.bookmarks.filter((bookmark) => bookmark.mangaId !== mangaId),
+            nextCursor: null,
+          }
+        }
+      })
+
+      if (isBookmarked) {
+        queryClient.invalidateQueries({ queryKey: QueryKeys.infiniteBookmarks })
+      }
+    },
+    shouldSetResponse: false,
+  })
 
   function handleClick(event: React.MouseEvent<HTMLButtonElement>) {
     if (!me) {
@@ -91,7 +91,7 @@ export default function BookmarkButton({ manga, className }: Readonly<Props>) {
   }
 
   return (
-    <form action={formAction} className={className}>
+    <form action={dispatchAction} className={className}>
       <input name="mangaId" type="hidden" value={mangaId} />
       <button
         className="flex justify-center items-center gap-1 w-full h-full transition disabled:bg-zinc-900 disabled:cursor-not-allowed"
