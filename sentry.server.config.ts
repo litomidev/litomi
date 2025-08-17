7f133173d3007ce4ba2402fcfb4814943afe313f
gwak2837 <gwak2837@gmail.com>
2025-08-17 16:19:41 +0900

    fix: 센트리 로컬 요청 방지

--- sentry.server.config.ts
+++ sentry.server.config.ts
@@ -5,7 +5,7 @@
 import * as Sentry from '@sentry/nextjs'
 
 Sentry.init({
-  dsn: 'https://40e7a8fce2e957f313b4bbe7b9c46a2e@o4506216356511744.ingest.us.sentry.io/4508939892097024',
+  dsn: process.env.NEXT_PUBLIC_SENTRY_DSN,
 
   // Define how likely traces are sampled. Adjust this value in production, or use tracesSampler for greater control.
   tracesSampleRate: 1,
