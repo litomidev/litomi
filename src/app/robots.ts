7f0ba8fcc58f43241454033edf17649fc2bf72c6
gwak2837 <gwak2837@gmail.com>
2025-08-18 13:35:31 +0900

    Update robots.ts

--- src/app/robots.ts
+++ src/app/robots.ts
@@ -7,7 +7,7 @@ export default function robots(): MetadataRoute.Robots {
     rules: {
       userAgent: '*',
       allow: '/',
-      disallow: '/api',
+      disallow: ['/api', '/test', '/@', '/search?*'],
     },
     sitemap: CANONICAL_URL + '/sitemap.xml',
   }
