a72ccd913d3eabf1179cdc0128005713308b26e5
gwak2837 <gwak2837@gmail.com>
2025-08-24 12:01:40 +0900

    Create SelectableMangaCard.tsx

---
+++ src/app/(navigation)/library/[id]/SelectableMangaCard.tsx
@@ -0,0 +1,32 @@
+import { Check } from 'lucide-react'
+import { memo } from 'react'
+
+import MangaCard from '@/components/card/MangaCard'
+import { Manga } from '@/types/manga'
+
+import { useLibrarySelectionStore } from './librarySelection'
+
+type Props = {
+  index: number
+  manga: Manga
+}
+
+export default memo(SelectableMangaCard)
+
+function SelectableMangaCard({ index, manga }: Readonly<Props>) {
+  const { selectedItems, toggleSelection } = useLibrarySelectionStore()
+  const isSelected = selectedItems.has(manga.id)
+
+  return (
+    <div
+      aria-selected={isSelected}
+      className="relative cursor-pointer aria-selected:ring-2 aria-selected:ring-brand-end rounded-xl"
+      onClick={() => toggleSelection(manga.id)}
+    >
+      <div className="absolute top-2 left-2 z-10 size-5 flex items-center justify-center rounded border-2 border-white bg-zinc-900/80">
+        {isSelected && <Check className="size-4" />}
+      </div>
+      <MangaCard className="h-full pointer-events-none" index={index} manga={manga} />
+    </div>
+  )
+}
