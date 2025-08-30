8e2af91932ba7c41f4e7977a0f26128d7244927a
gwak2837 <gwak2837@gmail.com>
2025-08-30 19:34:17 +0900

    fix: 뷰어 들어오면 최신 감상 기록으로 갱신

--- src/components/ImageViewer/ResumeReadingToast.tsx
+++ src/components/ImageViewer/ResumeReadingToast.tsx
@@ -1,8 +1,11 @@
 'use client'
 
+import { useQueryClient } from '@tanstack/react-query'
 import ms from 'ms'
 import { useEffect } from 'react'
 import { toast } from 'sonner'
+
+import { QueryKeys } from '@/constants/query'
 
 import { useImageIndexStore } from './store/imageIndex'
 import useReadingHistory from './useReadingHistory'
@@ -15,7 +18,9 @@ export default function ResumeReadingToast({ mangaId }: Readonly<Props>) {
   const getImageIndex = useImageIndexStore((state) => state.getImageIndex)
   const navigateToImageIndex = useImageIndexStore((state) => state.navigateToImageIndex)
   const { lastPage } = useReadingHistory(mangaId)
+  const queryClient = useQueryClient()
 
+  // NOTE: 읽은 페이지 토스트 표시
   useEffect(() => {
     if (lastPage && lastPage !== getImageIndex() + 1) {
       const toastId = toast(`마지막으로 읽던 페이지 ${lastPage}`, {
@@ -31,6 +36,11 @@ export default function ResumeReadingToast({ mangaId }: Readonly<Props>) {
       }
     }
   }, [lastPage, navigateToImageIndex, getImageIndex])
+
+  // NOTE: 뷰어 들어오면 최신 감상 기록으로 갱신
+  useEffect(() => {
+    queryClient.invalidateQueries({ queryKey: QueryKeys.readingHistory(mangaId) })
+  }, [mangaId, queryClient])
 
   return null
 }
