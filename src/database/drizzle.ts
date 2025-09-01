7d1f961bbbf92acd66fdb48f7a7457d008b31def
gwak2837 <gwak2837@gmail.com>
2025-09-01 23:54:23 +0900

    Update drizzle.ts

--- src/database/drizzle.ts
+++ src/database/drizzle.ts
@@ -4,6 +4,5 @@ import 'server-only'
 
 import { POSTGRES_URL } from '@/constants/env'
 
-// NOTE: Transaction이 필요하지 않은 pooling 연결
 export const client = postgres(POSTGRES_URL, { prepare: false })
 export const db = drizzle(client)
