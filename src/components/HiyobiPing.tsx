ffebe5908206854d92b1215b60147086b399106f
gwak2837 <gwak2837@gmail.com>
2025-09-18 16:07:54 +0900

    Update HiyobiPing.tsx

--- src/components/HiyobiPing.tsx
+++ src/components/HiyobiPing.tsx
@@ -4,7 +4,7 @@ import { useEffect } from 'react'
 
 export default function HiyobiPing() {
   useEffect(() => {
-    fetch('https://api-kh.hiyobi.org/hiyobi/ping', { headers: { referer: 'https://hiyobi.org/' } })
+    fetch('https://api-kh.hiyobi.org/hiyobi/ping', { headers: { refererPolicy: 'no-referrer' } }).catch(() => null)
   }, [])
 
   return null
