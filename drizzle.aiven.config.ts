7f64bd613adf4043262106bfe93e2230c9c3c4f1
gwak2837 <gwak2837@gmail.com>
2025-09-19 15:07:28 +0900

    feat: prod 환경 구분

--- drizzle.aiven.config.ts
+++ drizzle.aiven.config.ts
@@ -1,5 +1,5 @@
 import dotenv from 'dotenv'
-dotenv.config({ path: '.env.local' })
+dotenv.config({ path: process.env.DB_ENV === 'prod' ? '.env.prod' : '.env.local' })
 
 import { defineConfig } from 'drizzle-kit'
 
