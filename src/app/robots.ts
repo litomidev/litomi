f3ed1931909f43e56891b5eb412570442bbfc116
gwak2837 <gwak2837@gmail.com>
2025-09-21 00:42:11 +0900

    fix: sitemap, robot

--- src/app/robots.ts
+++ src/app/robots.ts
@@ -7,7 +7,6 @@ export default function robots(): MetadataRoute.Robots {
     rules: {
       userAgent: '*',
       allow: '/',
-      disallow: ['/api', '/test', '/@', '/search?*'],
     },
     sitemap: CANONICAL_URL + '/sitemap.xml',
   }
