98f859cceaa516ec73d47b6bc682867421c2c594
gwak2837 <gwak2837@gmail.com>
2025-10-12 23:57:33 +0900

    feat: 페이지 캐시 설정

--- cloudflare/terraform/cache-rules.tf
+++ cloudflare/terraform/cache-rules.tf
@@ -21,6 +21,25 @@ resource "cloudflare_ruleset" "cache_rules" {
           mode = "respect_origin"
         }
       }
+    },
+    {
+      ref         = "manga_pages_html"
+      enabled     = true
+      description = "Cache manga viewer HTML pages"
+      expression  = "(starts_with(http.request.uri.path, \"/manga/\") and not http.request.uri.path contains \".\" and not http.request.uri.path contains \"/detail\")"
+      action      = "set_cache_settings"
+
+      action_parameters = {
+        cache = true
+        edge_ttl = {
+          mode    = "override_origin"
+          default = 2592000 # 30 days
+        }
+        browser_ttl = {
+          mode    = "override_origin"
+          default = 3600 # 1 hour
+        }
+      }
     }
   ]
 }
