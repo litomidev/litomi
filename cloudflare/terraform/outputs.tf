04f74a09b89148c1accf2ff92a94e172126ef643
gwak2837 <gwak2837@gmail.com>
2025-10-13 11:15:12 +0900

    feat: rate limit 규칙 추가

--- cloudflare/terraform/outputs.tf
+++ cloudflare/terraform/outputs.tf
@@ -12,3 +12,18 @@ output "cache_rules_count" {
   description = "Number of cache rules configured"
   value       = length(cloudflare_ruleset.cache_rules.rules)
 }
+
+output "rate_limiting_ruleset_id" {
+  description = "ID of the rate limiting ruleset"
+  value       = cloudflare_ruleset.rate_limiting.id
+}
+
+output "rate_limiting_ruleset_name" {
+  description = "Name of the rate limiting ruleset"
+  value       = cloudflare_ruleset.rate_limiting.name
+}
+
+output "rate_limiting_rules_count" {
+  description = "Number of rate limiting rules configured"
+  value       = length(cloudflare_ruleset.rate_limiting.rules)
+}
