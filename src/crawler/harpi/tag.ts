b545dc81fd85f621ce3aa2c8b762259855583cb7
gwak2837 <gwak2837@gmail.com>
2025-10-15 19:33:25 +0900

    update: tag 종류

--- src/crawler/harpi/tag.ts
+++ src/crawler/harpi/tag.ts
@@ -6,3 +6,6 @@ type HarpiTag = {
 }
 
 export const HARPI_TAG_MAP: Record<string, HarpiTag> = harpiTagJSON
+
+// const a = [{ ... }]
+// Object.fromEntries(a.map(aa => [aa.id, { ko: aa.korStr, en: aa.engStr.replaceAll(' ', '_'),  }]))
